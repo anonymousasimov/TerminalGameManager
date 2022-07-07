afa410cb6e0b6a17d32a8963e8dae15aa8ba9fd7
anonymousasimov <spaldings31@gmail.com>
2022-07-08 00:20:59 +0100

    Minor Changes

--- tgm/tgm.py
+++ tgm/tgm.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 from rich.console import Console
 from rich.table import Table
 from rich import box
@@ -8,11 +6,14 @@ from pyfiglet import figlet_format as pyfig
 
 console = Console()
 
-titleTable = Table (
-    style = "#E1AD01",
-    box=box.SIMPLE_HEAVY, 
-)
+def display_menu():
+    titleTable = Table (
+        style = "#E1AD01",
+        box=box.SIMPLE_HEAVY, 
+    )
 
-titleTable.add_column(pyfig("Terminal Games"), style="#E1AD01")
-#table_print(titleTable)
-console.print(titleTable)
+    titleTable.add_column(pyfig("Terminal Games"), style="#E1AD01")
+
+    console.print(titleTable)
+
+display_menu()
