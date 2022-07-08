cf48f524c521ed97e439f9d3db11f5ec54ffc0d7
anonymousasimov <spaldings31@gmail.com>
2022-07-08 11:38:28 +0100

    Adds terminalGames.json and adds display of all terminal games listed in terminalGames.json to tgm.py display_menu()

--- tgm/tgm.py
+++ tgm/tgm.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python
+import os
+import json
+from random import gammavariate
+
 from rich.console import Console
 from rich.table import Table
 from rich import box
@@ -6,6 +11,14 @@ from pyfiglet import figlet_format as pyfig
 
 console = Console()
 
+def getGames() -> dict:
+    __location__ = os.path.realpath(
+        os.path.join(os.getcwd(), os.path.dirname(__file__))
+    )
+    with open(os.path.join(__location__, "terminalGames.json"), "r") as tg:
+        terminalGames = json.load(tg)
+    return terminalGames
+
 def display_menu():
     titleTable = Table (
         style = "#E1AD01",
@@ -13,6 +26,12 @@ def display_menu():
     )
 
     titleTable.add_column(pyfig("Terminal Games"), style="#E1AD01")
+    titleTable.add_column("id", style="#E1AD01")
+
+    i = 0
+    for game in getGames():
+        titleTable.add_row(game["title"],str(i))
+        i += 1
 
     console.print(titleTable)
 
